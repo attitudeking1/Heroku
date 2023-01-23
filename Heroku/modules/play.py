89d9c10a2ed78ff61ff453bf14c07ca134b104f5
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2023-01-24 00:03:12 +0530

    Update play.py

--- Heroku/modules/play.py
+++ Heroku/modules/play.py
@@ -576,6 +576,7 @@ async def play(_, message: Message):
             caption="ꜱᴏɴɢ ɪɴ Qᴜᴇᴜᴇ #{}\n**📂 ᴛɪᴛʟᴇ:**[{}]({})\n\n👥 ᴀᴅᴅᴇᴅ ʙʏ: {}".format(
                 position, title, url, message.from_user.mention()
             ),
+        )
             keyboard = InlineKeyboardMarkup(
     [
         
@@ -590,7 +591,6 @@ async def play(_, message: Message):
         
     ]
 )
-        )
     else:
         try:
             await calls.pytgcalls.join_group_call(
