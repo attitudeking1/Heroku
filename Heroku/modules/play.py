4b6b66d38b7be9462f1f5d719b3b39c8a1731072
Tushar Gadhe <80459967+attitudeking1@users.noreply.github.com>
2023-01-24 00:26:01 +0530

    Update play.py

--- Heroku/modules/play.py
+++ Heroku/modules/play.py
@@ -576,7 +576,7 @@ async def play(_, message: Message):
             caption="ꜱᴏɴɢ ɪɴ Qᴜᴇᴜᴇ #{}\n**📂 ᴛɪᴛʟᴇ:**[{}]({})\n\n👥 ᴀᴅᴅᴇᴅ ʙʏ: {}".format(
                 position, title, url, message.from_user.mention()
             ),
-            keyboard = InlineKeyboardMarkup(
+            reply_markup=InlineKeyboardMarkup(
     [
         
        [
